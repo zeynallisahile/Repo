9ad341dffcf59148be95f11dfa1f671d6f50a3a6
zeynallisahile <tu7dfbyuu@code.edu.az>
2022-04-01 11:57:48 +0400

    Task Completed

---
+++ ConsoleApp11/ConsoleApp11/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+        private int _no;
+        public int No { get=>_no;}
+
+        public Book()
+        {
+            _no++;
+        }
+    }
+}
---
+++ ConsoleApp11/ConsoleApp11/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class Library
+    {
+        List<int> Books = new List<int>();
+
+    }
+}
